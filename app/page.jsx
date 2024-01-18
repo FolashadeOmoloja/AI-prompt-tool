ca1495b4365ae743269e3bac2577753e7efa09c7
Folashade Omoloja <102021983+FolashadeOmoloja@users.noreply.github.com>
2024-01-18 23:43:04 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,11 @@ import MainComp from '../components/MainComp'
 
 const Home = () => {
   return (
-    <><div>page</div><MainComp /></>
+    <>
+      <div>page</div>
+      <MainComp />
+      <div>Placeholder container </div>
+    </>
   )
 }
 
